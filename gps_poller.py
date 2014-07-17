c1bd96bd4d528436261e3a556e80d0b034f074d7
Qiuxi Zhu <zhuqiuxizju@gmail.com>
2014-07-16 22:40:45 -0700

    Dictionary encaps

--- gps_poller.py
+++ gps_poller.py
@@ -13,63 +13,89 @@ class GPSPoller(Thread):
 		while True:
 			self.gpsd.next()
 	
-	def latitude(self):
+	def lat(self):
 		return self.gpsd.fix.latitude
 	
-	def longitude(self):
+	def lon(self):
 		return self.gpsd.fix.longitude
 	
-	def altitude(self):
+	def alt(self):
 		return self.gpsd.fix.altitude
 	
-	def eps(self):
-		return self.gpsd.fix.eps
-	
-	def epx(self):
-		return self.gpsd.fix.epx
-
-	def epv(self):
-		return self.gpsd.fix.epv
-	
-	def ept(self):
-		return self.gpsd.fix.ept
-	
+	# Ground speed (m/sec)
 	def speed(self):
 		return self.gpsd.fix.speed
 	
+	# Climb rate (m/sec)
 	def climb(self):
 		return self.gpsd.fix.climb
 
+	# Course over ground from true north (deg)
 	def track(self):
 		return self.gpsd.fix.track
+	
+	# UTC time string
+	# def utc(self):
+	# 	return self.gpsd.utc
+	
+	# Estimated longitude error (m)
+	def epx(self):
+		return self.gpsd.fix.epx
 
+	# Estimated latitude error (m)
+	def epy(self):
+		return self.gpsd.fix.epy
+
+	# Estimated vertical error (m/sec)
+	def epv(self):
+		return self.gpsd.fix.epv
+	
+	# Estimated speed error (m/sec)
+	def eps(self):
+		return self.gpsd.fix.eps
+
+	# Estimated course error (deg)
+	def epd(self):
+		return self.gpsd.fix.epd
+
+	# Estimated time-stamp error (sec)
+	def ept(self):
+		return self.gpsd.fix.ept
+
+	# Fix mode
 	def mode(self):
 		return self.gpsd.fix.mode
-	
-	def utc(self):
-		return self.gpsd.utc
 
-MAIN_HD_SP = 15
-MAIN_SLEEP = 1.0
+	MODE_STR = {
+		0: "NO DATA",
+		1: "NO FIX",
+		2: "2D FIX",
+		3: "3D FIX"
+	}
+
+	def dict(self):
+		return {
+			"lat": self.lat(),
+			"lon": self.lon(),
+			"alt": self.alt(),
+			"speed": self.speed(),
+			"climb": self.climb(),
+			"track": self.track(),
+			"epx": self.epx(),
+			"epy": self.epy(),
+			"epv": self.epv(),
+			"eps": self.eps(),
+			"epd": self.epd(),
+			"ept": self.ept(),
+			"mode": self.mode()
+		}
+
+MAIN_SLEEP = 1
 
 if __name__ == "__main__":
 	poller = GPSPoller()
 	poller.daemon = True
 	poller.start()
 	while True:
-		os.system("clear")
-		print "GPS"
-		print
-		print ("%%-%ds") % MAIN_HD_SP % ("latitude"),  poller.latitude()
-		print ("%%-%ds") % MAIN_HD_SP % ("longitude"), poller.longitude()
-		print ("%%-%ds") % MAIN_HD_SP % ("altitude"),  poller.altitude()
-		print ("%%-%ds") % MAIN_HD_SP % ("eps"),       poller.eps()
-		print ("%%-%ds") % MAIN_HD_SP % ("epx"),       poller.epx()
-		print ("%%-%ds") % MAIN_HD_SP % ("epv"),       poller.epv()
-		print ("%%-%ds") % MAIN_HD_SP % ("ept"),       poller.ept()
-		print ("%%-%ds") % MAIN_HD_SP % ("speed"),     poller.speed()
-		print ("%%-%ds") % MAIN_HD_SP % ("climb"),     poller.climb()
-		print ("%%-%ds") % MAIN_HD_SP % ("track"),     poller.track()
-		print ("%%-%ds") % MAIN_HD_SP % ("mode"),      poller.mode()
-		print ("%%-%ds") % MAIN_HD_SP % ("time"),      poller.utc()
+		print poller.dict()
 		time.sleep(MAIN_SLEEP)
